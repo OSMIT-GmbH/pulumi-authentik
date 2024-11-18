2a23fccb590179ec233c6bbb28b6c6c981149438
Christian Nießner <christian.niessner@osmit.de>
2024-11-18 23:46:27 +0100

    [chore] fixed pipline issues

--- sdk/go/authentik/config/config.go
+++ sdk/go/authentik/config/config.go
@@ -4,7 +4,7 @@
 package config
 
 import (
-	"github.com/OSMIT-GmbH/pulumi-authentik/sdk/go/authentik/internal"
+	"github.com/OSMIT-GmbH/pulumi-authentik/sdk/v2024/go/authentik/internal"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
